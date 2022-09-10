53f5cec895b3743e83bafbf1fbb995ccda8557b3
Robert Parsons <rmpcoding@gmail.com>
2022-09-09 18:49:53 -0700

    Adds backend directory set up

--- server/server.js
+++ server/server.js
@@ -1,38 +1,32 @@
-// // Require external packages
-// require('dotenv').config();
-// require('./db/mongoose');
 const express = require('express');
-// const cors = require('cors');
-// const passport = require('passport');
-// const cookieParser = require('cookie-parser');
-// const path = require('path')
-// // Declare instance of Express
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 const app = express();
+const routes = require('./routes');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env')});
+require('./db/mongoose');
 
 // Declare value for PORT
 const PORT = process.env.PORT || 3001;
 
-// // Express middleware
-// app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static('client/build'));
-// if (process.env.PRODUCTION_URL) {
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build/index.html'));
-//   });
-// }
-// app.use(cookieParser());
-// app.use(passport.initialize());
-// app.use(passport.session());
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('client/build'));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// // Routes defined here
-// require('./routes/routes')(app);
-// require('./routes/recipes')(app);
-// require('./routes/spoonacular-api')(app);
+// Production server runs this code when config var is present
+if (process.env.PRODUCTION_URL) {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
+}
 
-
-
+// Routes defined here
+app.use(routes);
 
 // Set Express server to listen on PORT
 app.listen(PORT, () => {
